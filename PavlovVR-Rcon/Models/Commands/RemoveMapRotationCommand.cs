f1ed1f361014529c7adb467c99f51d2fc4684572
Longoon12000 <longoon12000@gmail.com>
2022-12-18 17:51:50 +0100

    Fixes wrong reply class

--- PavlovVR-Rcon/Models/Commands/RemoveMapRotationCommand.cs
+++ PavlovVR-Rcon/Models/Commands/RemoveMapRotationCommand.cs
@@ -1,6 +1,8 @@
-﻿namespace PavlovVR_Rcon.Models.Commands
+﻿using PavlovVR_Rcon.Models.Replies;
+
+namespace PavlovVR_Rcon.Models.Commands
 {
-    public class RemoveMapRotationCommand : BaseCommand<RemoveMapRotationCommand>
+    public class RemoveMapRotationCommand : BaseCommand<RemoveMapRotationReply>
     {
         public RemoveMapRotationCommand() : base("RemoveMapRotation")
         {
